304c1c4f9d7e3a179241c9056df40175c7999dc7
Dass-pe <sourav.das@pointestimation.com>
2021-07-01 00:53:15 +0100

    Aggregate map created

--- TestCode.py
+++ TestCode.py
@@ -5,7 +5,7 @@ churn_data = pd.DataFrame({'CustomerId': [1,2,3,4,5,6,7,8,9,10],
                            'Age': [35, 67, 55, 41, 76, 65, 89, 55, 25, 32]})
 
 aggmap = {'Age': 'mean',
-       'Churn': 'sum'}
+         'Churn': 'sum'}
 
 print(churn_data.groupby(['Gender']).agg(aggmap))
 print(churn_data.head())
